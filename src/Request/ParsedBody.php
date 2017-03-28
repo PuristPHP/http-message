e4eb46b0c7e23f64ae3661d44949739b9f6e5acf
Nicholas Ruunu <nicholas@ruu.nu>
2017-03-28 08:22:48 +0200

    Adds ParsedBody class

---
+++ src/Request/ParsedBody.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Purist\Request;
+
+use InvalidArgumentException;
+use stdClass;
+
+final class ParsedBody
+{
+    private $parsedBody;
+
+    public function __construct($parsedBody)
+    {
+        if (!is_array($parsedBody) && !$parsedBody instanceof stdClass) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'Parsed body of type %s must be either array or stdClass',
+                    gettype($parsedBody)
+                )
+            );
+        }
+
+        $this->parsedBody = $parsedBody;
+    }
+
+    public function get()
+    {
+        return $this->parsedBody;
+    }
+}
